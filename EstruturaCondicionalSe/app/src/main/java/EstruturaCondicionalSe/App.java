e64f6b258c7170b738b99eced70954e75ee5ef18
Jonatas Weverton <jonataswevertom@hotmail.com>
2023-01-29 09:42:13 -0300

    Estrutura condicional if else em Java

---
+++ EstruturaCondicionalSe/app/src/main/java/EstruturaCondicionalSe/App.java
@@ -0,0 +1,18 @@
+package EstruturaCondicionalSe;
+
+public class App {
+
+    public static void main(String[] args) {
+        int media = 7;
+
+        if (media >= 7) {
+            if (media == 10) {
+                System.out.println("Aprovado Nota Maxima!");
+            } else {
+                System.out.println("Aprovado");
+            }
+        } else {
+            System.out.println("Reprovado");
+        }
+    }
+}
