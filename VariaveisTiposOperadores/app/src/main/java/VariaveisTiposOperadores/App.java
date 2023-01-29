b77212a4bff89cf86179740bbe7228f637839bc5
Jonatas Weverton <jonataswevertom@hotmail.com>
2023-01-29 09:23:03 -0300

    Variaveis e tipos de operadores em java

---
+++ VariaveisTiposOperadores/app/src/main/java/VariaveisTiposOperadores/App.java
@@ -0,0 +1,24 @@
+package VariaveisTiposOperadores;
+
+public class App {
+   
+    public static void main(String[] args) {
+        int idade = 0;
+        float cotacaoDolar = 5.0f;
+        double cotacaoEuro = 6.0d;
+        char genero = 'M';
+        byte placar = 0;
+        boolean estaCadastrado = false;
+        
+        String nome = "Esse é um texto de uma variavel String";
+        
+        System.out.println(idade);
+        System.out.println(cotacaoDolar);
+        System.out.println(cotacaoEuro);
+        System.out.println(genero);
+        System.out.println(placar);
+        System.out.println(estaCadastrado);
+        System.out.println(nome);
+        
+    }
+}
