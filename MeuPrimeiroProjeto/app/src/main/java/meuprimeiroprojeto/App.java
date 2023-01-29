33c265abefa75271d996405cd285f7e2dd84e87c
Jonatas Weverton <jonataswevertom@hotmail.com>
2023-01-29 09:21:54 -0300

    Meu Primeiro Projeto Java

---
+++ MeuPrimeiroProjeto/app/src/main/java/meuprimeiroprojeto/App.java
@@ -0,0 +1,9 @@
+package meuprimeiroprojeto;
+
+public class App {
+   
+    public static void main(String[] args) {
+        System.out.println("Olá Mundo!");
+        System.out.println("Minha segunda mensagem!");
+    }
+}
